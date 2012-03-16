3dd8211c770b7e5ac696234629badfccded24daa
Willian <willian.gomes@gmail.com>
2012-03-16 00:03:59 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fudencio::Application.config.secret_token = '809abd85e84c024715883b80fe262fa77c13f269282981d58b2aa1f62717ed1ea370f4ee327a0f641036200017747972b5d8d22c99cb3546ac14f701266ec449'
